a11c4049e134bb44b3435ad1e3c49ac742dd23cb
jiuxia <497391872@qq.com>
2022-06-07 01:37:41 -0700

    handler push

--- cmd/api/handlers/handler.go
+++ cmd/api/handlers/handler.go
@@ -1,4 +1,25 @@
 package handlers
 
+import (
+	error2 "github.com/bdyc-org/dousheng/pkg/error"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Response struct {
+	Code int64       `json:"status_code"`
+	Msg  string      `json:"status_msg"`
+	Data interface{} `json:"data"`
+}
+
 type VideoParam struct {
+}
+
+func SendResponse(c *gin.Context, err error, data interface{}) {
+	Err := error2.ConvertErr(err)
+	c.JSON(http.StatusOK, Response{
+		Code: Err.ErrCode,
+		Msg:  Err.ErrMsg,
+		Data: data,
+	})
 }
--- cmd/api/handlers/publish_video.go
+++ cmd/api/handlers/publish_video.go
@@ -1,7 +1,25 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	error2 "github.com/bdyc-org/dousheng/pkg/error"
+	"github.com/gin-gonic/gin"
+	"path/filepath"
+)
 
 func PublishVideo(c *gin.Context) {
+	//TODO token judge
+	token := c.PostForm("token")
+	title := c.PostForm("title")
+	fmt.Println(token)
+	data, err := c.FormFile("data")
+	if err != nil {
+		SendResponse(c, error2.ConvertErr(err), nil)
+		return
+	}
+	filename := filepath.Base(data.Filename)
+	//TODO  filaname need to be changed
+	//finalName := fmt.Sprintf("%d_%s", user.Id, filename)
+	saveFile := filepath.Join("../../../public/", filename)
 
 }
